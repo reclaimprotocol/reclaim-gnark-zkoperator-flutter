6e54f52d7b7fdbcfef3a8cc3889510feb41964d6
predatorx7 <smushaheed@outlook.com>
2024-08-28 04:14:18 +0530

    Add script to build native lib for iOS

---
+++ scripts/build_native.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e;
+
+GO_GNARKPROVER_REPO_URL="https://$PACKAGE_CLONE_USER:$PACkAGE_CLONE_PASSWD@gitlab.reclaimprotocol.org/reclaim/gnark-symmetric-crypto";
+
+mkdir -p vendor;
+
+git clone $GO_GNARKPROVER_REPO_URL vendor/gnark-symmetric-crypto;
+
+export GO_GNARKPROVER_DIR="$(pwd)/vendor/gnark-symmetric-crypto";
+
+echo "Starting build for iOS"
+
+cd src;
+make ios;
+cd ../;
+
+# cleanup
+rm -rf $GO_GNARKPROVER_DIR;
+
+echo "Updating repository with new native libraries";
+git add ios;
+git commit -m "Update ios native library";
+git push;
+echo "Starting build for iOS"
