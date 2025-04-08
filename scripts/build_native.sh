c9e2afa2177b2bf8c91a943ef6a479c61d7b44d0
Mushaheed Syed <mushaheed@creatoros.co>
2025-04-08 19:08:43 +0530

    Add creation of pull request in build native script

--- scripts/build_native.sh
+++ scripts/build_native.sh
@@ -6,8 +6,8 @@ if [ -z "$GO_GNARKPROVER_DIR" ]
 then
     GO_GNARKPROVER_REPO_URL="https://github.com/reclaimprotocol/zk-symmetric-crypto";
     mkdir -p vendor;
-    git clone $GO_GNARKPROVER_REPO_URL vendor/gnark-symmetric-crypto;
-    export GO_GNARKPROVER_DIR="$(pwd)/vendor/gnark-symmetric-crypto/gnark";
+    git clone $GO_GNARKPROVER_REPO_URL vendor/zk-symmetric-crypto;
+    export GO_GNARKPROVER_DIR="$(pwd)/vendor/zk-symmetric-crypto/gnark";
 fi
 
 ./scripts/build_lib.sh
@@ -17,17 +17,27 @@ rm -rf $GO_GNARKPROVER_DIR;
 
 echo "Updating repository with new native libraries";
 
-bash ./scripts/update_version.sh;
+bash ./scripts/update_version.sh --minor;
 
 get_timestamp() {
     date "+%Y%m%d%H%M"
 }
 
-BUILD_BRANCH="build-$(get_timestamp)"
+export BUILD_BRANCH="build-$(get_timestamp)"
 git checkout -b $BUILD_BRANCH;
 
 git add pubspec.yaml;
 
-git commit -m "Update [CI] native libraries for Android & iOS";
+BUILD_COMMIT_MESSAGE="Update [CI] native libraries for Android & iOS";
+
+git commit -m "$BUILD_COMMIT_MESSAGE";
 git push --set-upstream origin $BUILD_BRANCH;
 git push;
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $PACkAGE_CLONE_PASSWD" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/reclaimprotocol/reclaim-gnark-zkoperator-flutter/pulls \
+  -d '{"title":"'$BUILD_COMMIT_MESSAGE'","body":"Updated native libraries built by CI from latest changes in the zk-symmetric-crypto repository","head":"'$BUILD_BRANCH'","base":"main"}'
