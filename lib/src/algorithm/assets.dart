0b98e4751a30bb536bb8aeefe82fc00d24def905
Mushaheed Syed <mushaheed.syed@proton.me>
2025-10-09 14:01:31 +0530

    Update [CI] native libraries for Android & iOS (#11)
    * Update [CI] native libraries for Android & iOS
    
    * Update new gnark assets download path

--- lib/src/algorithm/assets.dart
+++ lib/src/algorithm/assets.dart
@@ -2,8 +2,8 @@ import 'algorithm.dart';
 
 const _gnarkAssetMirrors = [
   // https://github.com/reclaimprotocol/zk-symmetric-crypto/raw/refs/heads/main/resources/gnark
-  'https://d5znggfgtutzp.cloudfront.net',
-  'https://reclaim-gnark-assets.rough-hat-079e.workers.dev',
+  'https://d5znggfgtutzp.cloudfront.net/operator-circuits-v3',
+  'https://reclaim-gnark-assets.rough-hat-079e.workers.dev/operator-circuits-v3',
   'https://github.com/reclaimprotocol/zk-symmetric-crypto/raw/refs/heads/main/resources/gnark',
 ];
 
